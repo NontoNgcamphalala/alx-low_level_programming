9b9c0eaad4ddfc5975cd6fc034e195e441d9bcde
Nontobeko <nontodlalose@gmail.com>
2023-02-16 18:56:46 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ *
  * main - prints "with proper grammar, but the outcome is a piece of art,"
  * ending in new line.
  * Return: Always 0 (Success)
