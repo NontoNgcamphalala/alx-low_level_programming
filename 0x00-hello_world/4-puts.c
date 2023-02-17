c7ac950fe0808f5e25ad0a372443d5b0e18149cb
Nontobeko <nontodlalose@gmail.com>
2023-02-16 16:10:14 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/** 
+ * Program to print "Programming is like building a multilingual puzzle" 
+ * using puts 
+ * returning 0  
+ */
+
+int main() {
+  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
