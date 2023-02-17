304bdfdc73ecf74fdb20e1daba8a71343f0dd7d9
Nontobeko <nontodlalose@gmail.com>
2023-02-16 16:51:34 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,10 @@
  * main - prints "with proper grammar, but the outcome is a piece of art,"
  * ending in new line.
  * Returning 0.
+ *
  */
 
-int main() {
+int main(void) {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
