3f739dffaaa8e25b325c39c2d21f58d2bf316225
Nontobeko <nontodlalose@gmail.com>
2023-02-16 18:50:17 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main -  print "Programming is like building a multilingual puzzle
  * by a new line"
- * returns 0.
+ * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(main)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
