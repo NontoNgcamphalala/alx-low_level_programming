64dca3a9d864a0a1359a082e1e5004acc5e93b82
Nontobeko <nontodlalose@gmail.com>
2023-02-17 16:47:12 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -14,7 +14,7 @@ int main(void)
 
 	for (d = '0'; d <= '9'; d++)
 	putchar(d);
-	putchar('\n); 
-
+	putchar('\n'); 
 	return (0);
+
 }
