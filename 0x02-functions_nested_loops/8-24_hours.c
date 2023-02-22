45cae5d8b7f8ed61a95321ef3eecb41891a15f23
Nontobeko <nontodlalose@gmail.com>
2023-02-21 21:18:59 -0800

    I'm federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 		{
 			_putchar((b / 10) + '0');
 			_putchar((b % 10) + '0');
-			_putchar(';');
+			_putchar(':');
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar('\n');
