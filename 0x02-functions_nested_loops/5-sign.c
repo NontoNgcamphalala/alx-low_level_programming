8c4130c8bdcaddac2c65eb96412f9580d6ffbee1
Nontobeko <nontodlalose@gmail.com>
2023-02-22 06:06:49 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,16 +13,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(43);
-	return (1);
-	} else if (n < 0)
-	{
-	_putchar(45);
-	return (-1);
-	}	
-	else
-	{	
-	_putchar(45);
-	return (0);
-	}
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
 }
