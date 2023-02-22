383ea092e844eb9a78298f11819d4759a92e340f
Nontobeko <nontodlalose@gmail.com>
2023-02-21 20:56:23 -0800

    I'm federal agent Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+
+{
+	int b, h;
+
+	b = 0;
+
+	while (b < 24)
+	{
+		h = 0;
+		while (h < 60)
+		{
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar(';');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar('\n');
+			h++;
+		}
+		b++;
+	}
+}
+
