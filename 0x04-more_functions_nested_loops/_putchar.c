e608d0e230187ab22bce1a007a0a8a08324841ce
Nontobeko <nontodlalose@gmail.com>
2023-02-24 12:10:04 -0800

    isdigit and Collaboration is multiplication

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -5,8 +5,8 @@
  * _putchar -writes the character c to stdout
  * @c: the character to print
  *
- * Return: 1 On Success
- * ON Error, return -1 and errno is set approprietly.
+ * Return: On Success 1
+ * on error, return -1 and errno is set approprietly.
  */
 
 int _putchar(char c)
