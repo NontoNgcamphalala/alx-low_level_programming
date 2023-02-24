894e0ce6977fa9aaf9d212a38e3bad466242b66d
Nontobeko <nontodlalose@gmail.com>
2023-02-23 18:47:36 -0800

    isdigit and Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 int
+ * @a: is first int
+ * @b: is second int
+ * Return: product of multiplication
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
