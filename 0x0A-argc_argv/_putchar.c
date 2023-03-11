1ea4543a54f3e09436137bfde6e60c1a32ddf109
Nontobeko <nontodlalose@gmail.com>
2023-03-10 22:39:46 -0800

    Mandatory Task 0 1 2 3 4

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar -writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On Success 1
+ * on error, return -1 and errno is set approprietly.
+ */
+
+int _putchar(char c)
+
+{
+        return (write(1, &c, 1));
+}
